a579992a160c3568160a4305e454e119abd1c012
Abdulla <mi3jg2@inf.elte.hu>
2021-11-27 17:35:25 +0100

    new footer design

--- components/Card.tsx
+++ components/Card.tsx
@@ -43,14 +43,14 @@ export default function Card() {
   return (
     <div className="Card">
       <div className="offer-images">
-        <div className="btns">
+        {/* <div className="btns">
           <div className="carousel-btn prev-btn">
             <button>P</button>
           </div>
           <div className="carousel-btn next-btn">
             <button>N</button>
           </div>
-        </div>
+        </div> */}
         <div className="carousel">
           <Image
             src="/DSC_2778.jpg"
@@ -60,41 +60,51 @@ export default function Card() {
           />
           <Slider {...settings}>
             <div className="slider">
-              <Image
-                className="im"
-                src="/DSC_2778.jpg"
-                alt="Offer Image"
-                objectFit="cover"
-                layout="fill"
-              />
-              <Image
-                className="im"
-                src="/DSC_2778.jpg"
-                alt="Offer Image"
-                objectFit="cover"
-                layout="fill"
-              />
-              <Image
-                className="im"
-                src="/DSC_2778.jpg"
-                alt="Offer Image"
-                objectFit="cover"
-                layout="fill"
-              />
-              <Image
-                className="im"
-                src="/DSC_2778.jpg"
-                alt="Offer Image"
-                objectFit="cover"
-                layout="fill"
-              />
-              <Image
-                className="im"
-                src="/DSC_2778.jpg"
-                alt="Offer Image"
-                objectFit="cover"
-                layout="fill"
-              />
+              <div>
+                <Image
+                  className="im"
+                  src="/DSC_2778.jpg"
+                  alt="Offer Image"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div>
+                <Image
+                  className="im"
+                  src="/DSC_2778.jpg"
+                  alt="Offer Image"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div>
+                <Image
+                  className="im"
+                  src="/DSC_2778.jpg"
+                  alt="Offer Image"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div>
+                <Image
+                  className="im"
+                  src="/DSC_2778.jpg"
+                  alt="Offer Image"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
+              <div>
+                <Image
+                  className="im"
+                  src="/DSC_2778.jpg"
+                  alt="Offer Image"
+                  objectFit="cover"
+                  layout="fill"
+                />
+              </div>
               <Image
                 className="im"
                 src="/DSC_2778.jpg"
