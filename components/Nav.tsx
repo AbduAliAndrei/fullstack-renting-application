bc237b8766bcd416dce843d65620e94833f99bf3
Abdulla <mi3jg2@inf.elte.hu>
2021-12-08 13:04:33 +0100

    implement different NAV bar when user is logged in

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/dist/client/router";
+import Image from "next/image";
 
 // import your icons
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLinkProps } from "react-bootstrap";
 import Button from "@material-ui/core/Button";
+import Typography from "@mui/material/Typography";
 
 function LoggedInUserNav() {
   const router = useRouter();
@@ -47,30 +49,51 @@ function LoggedInUserNav() {
           </ul>
         </nav>
       </div>
-      {/* <div className="login_signup">
-        <div className="login">
-          <Link href="/login">
-            <Button disableElevation className="logInBtn" variant="contained">
-              Log In
-            </Button>
-          </Link>
+      <div className="profileDetails">
+        <div className="primaryInfo">
+          <div className="imgContainer">
+            <Image
+              className="userProfileImg"
+              src="/budapest2.jpg"
+              alt="Image"
+              objectFit="cover"
+              layout="fill"
+            />
+          </div>
+          <Typography className="userName" variant="h6" component="h6">
+            Abdulla
+          </Typography>
         </div>
-        <div className="signup">
-          <Link href="/register">
-            <Button disableElevation className="signUpBtn" variant="contained">
-              Sign Up
-            </Button>
-          </Link>
+        <div className="userMenu">
+          <nav>
+            <ul>
+              <li className={router.pathname === "/" ? "active" : ""}>
+                <Link href="/profile">
+                  <a href="">My offers</a>
+                </Link>
+              </li>
+              <li className={router.pathname === "/offers" ? "active" : ""}>
+                <Link href="/profile">
+                  <a href="">Settings</a>
+                </Link>
+              </li>
+              <li className={router.pathname === "/contact" ? "active" : ""}>
+                <Link href="/contact">
+                  <a href="">Log out</a>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
 export default function Nav({}: NavLinkProps) {
   const router = useRouter();
-  const userLoggedIn = false;
+  const userLoggedIn = true;
   return userLoggedIn ? (
-    LoggedInUserNav()
+    <LoggedInUserNav />
   ) : (
     <div className="Nav">
       <input type="checkbox" id="check" />
