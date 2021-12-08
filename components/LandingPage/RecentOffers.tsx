e3ba8da7c24014289dee86fb6669af7cecf8a405
Abdulla <mi3jg2@inf.elte.hu>
2021-12-08 02:55:40 +0100

    responsive header, footer and offers page

--- components/LandingPage/RecentOffers.tsx
+++ components/LandingPage/RecentOffers.tsx
@@ -1,18 +1,33 @@
-// import '../styles/App.css'
+import Card from "../../components/Card";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import React from "react";
 export default function RecentOffers() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    swipe: false,
+  };
   return (
     <div className="RecentOffers">
       <div className="title">
         <h1>Recent Offers</h1>
       </div>
 
-      <div className="offers">
-        <div className="offer" />
-        <div className="offer" />
-        <div className="offer" />
-        <div className="offer" />
-      </div>
+      <Slider className="slider-container" {...settings}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Slider>
     </div>
   );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,5 @@
 import Auth from "../components/Auth";
-import React, { FormEvent, useCallback } from "react";
+import React, { FormEvent, useCallback, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useRouter } from "next/router";
 import Button from "@mui/material/Button";
@@ -14,8 +14,311 @@ import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormHelperText from "@mui/material/FormHelperText";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+import { dividerClasses } from "@mui/material";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Search from "@material-ui/icons/Search";
+
+let generalInfo = () => {
+  return (
+    <div>
+      <div className="profileImageContainer">
+        <Typography className="profileImageTitle" variant="h5" component="h5">
+          Profile image
+        </Typography>
+        <div className="profileImage-extraInfo">
+          <div className="profileImage">
+            <Image
+              src="/DSC_2778.jpg"
+              alt="Landlord Image"
+              className="ld-img"
+              objectFit="cover"
+              layout="fill"
+              priority={true}
+            />
+          </div>
+          <div className="editBtns-note">
+            <div className="editBtns">
+              <Button
+                disableElevation
+                className="changeAvatar"
+                variant="contained"
+              >
+                Change Avatar
+              </Button>
+              <Button
+                disableElevation
+                className="deleteAvatar"
+                variant="contained"
+              >
+                delete Avatar
+              </Button>
+            </div>
+            <Typography className="note" variant="h6" component="h6">
+              *If you will delete your avatar you will lose your trusted status
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <div className="primaryInfo">
+        <Typography className="title" variant="h5" component="h5">
+          Primary Information
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className="primaryInformationForm"
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <TextField
+              label="First name"
+              className="infoInput"
+              id="standard-size-normal"
+              defaultValue="Normal"
+              variant="standard"
+              InputProps={{ disableUnderline: true }}
+            />
+            <TextField
+              className="infoInput"
+              label="Last Name"
+              id="standard-size-normal"
+              defaultValue="Normal"
+              variant="standard"
+              InputProps={{ disableUnderline: true }}
+            />
+          </div>
+          <div>
+            <TextField
+              className="infoInput"
+              label="Location"
+              id="standard-size-normal"
+              defaultValue="Normal"
+              variant="standard"
+              InputProps={{ disableUnderline: true }}
+            />
+          </div>
+          <div>
+            <Typography variant="h6" component="h6">
+              Turn on GPS location
+              <Switch color="primary" />
+            </Typography>
+          </div>
+        </Box>
+      </div>
+      <div className="secondaryInfo">
+        <Typography className="title" variant="h5" component="h5">
+          Secondary Information
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className="secondaryInformationForm"
+        >
+          <FormControl sx={{ minWidth: 320 }}>
+            <Select
+              value={1}
+              displayEmpty
+              inputProps={{ "aria-label": "Without label" }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+            <FormHelperText>Without label</FormHelperText>
+          </FormControl>
+          <div>
+            <Typography className="title" variant="h5" component="h5">
+              Bio
+            </Typography>
+            <TextareaAutosize
+              maxRows={4}
+              aria-label="maximum height"
+              placeholder="100 words maximum."
+              style={{
+                width: 500,
+                border: "none",
+                padding: "10px",
+                minHeight: "200px",
+                outline: "none",
+              }}
+            />
+          </div>
+          <div>
+            <Button
+              disableElevation
+              className="deleteAvatar"
+              variant="contained"
+            >
+              Save changes
+            </Button>
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+let passwordInfo = () => {
+  return (
+    <div>
+      <div className="primaryInfo">
+        <Typography className="title" variant="h5" component="h5">
+          Password
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className="primaryInformationForm"
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type a location"
+            />
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type a location"
+            />
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type a location"
+            />
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+let emailInfo = () => {
+  return (
+    <div>
+      <div className="primaryInfo">
+        <Typography className="title" variant="h5" component="h5">
+          Email
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className="primaryInformationForm"
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Typography variant="h6" component="h6">
+              Old Email
+            </Typography>
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type your old email"
+            />
+            <Typography variant="h6" component="h6">
+              New Email
+            </Typography>
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type your new email"
+            />
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+let roleChangeInfo = () => {
+  return (
+    <div>
+      <div className="primaryInfo">
+        <Typography className="title" variant="h5" component="h5">
+          Primary Information role change
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+          className="primaryInformationForm"
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type a location"
+            />
+            <TextField
+              id="input-with-icon-textfield"
+              InputProps={{
+                disableUnderline: true,
+              }}
+              style={{
+                backgroundColor: "#fff",
+              }}
+              placeholder="Type a location"
+            />
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+};
 
 const Profile = () => {
+  const [profileContent, setProfileContent] = useState(() => generalInfo());
   const [xsrfToken] = useCookies(["XSRF-TOKEN"]);
   const router = useRouter();
 
@@ -42,161 +345,52 @@ const Profile = () => {
     <Auth>
       <div className="Profile">
         <div className="controlBtns">
-          <Button disableElevation className="controlBtn" variant="contained">
+          <Button
+            disableElevation
+            className="controlBtn"
+            variant="contained"
+            onClick={() => {
+              setProfileContent(generalInfo);
+            }}
+          >
             General Info
           </Button>
-          <Button disableElevation className="controlBtn" variant="contained">
+          <Button
+            disableElevation
+            className="controlBtn"
+            variant="contained"
+            onClick={() => {
+              setProfileContent(passwordInfo);
+            }}
+          >
             Password
           </Button>
-          <Button disableElevation className="controlBtn" variant="contained">
+          <Button
+            disableElevation
+            className="controlBtn"
+            variant="contained"
+            onClick={() => {
+              setProfileContent(emailInfo);
+            }}
+          >
             Email
           </Button>
-          <Button disableElevation className="controlBtn" variant="contained">
+          <Button
+            disableElevation
+            className="controlBtn"
+            variant="contained"
+            onClick={() => {
+              setProfileContent(roleChangeInfo);
+            }}
+          >
             Role Change
           </Button>
           <form onSubmit={onLogout}>
             <button id="logoutBtn">Logout</button>
           </form>
         </div>
-        <div className="profileImageContainer">
-          <Typography className="profileImageTitle" variant="h5" component="h5">
-            Profile image
-          </Typography>
-          <div className="profileImage-extraInfo">
-            <div className="profileImage">
-              <Image
-                src="/DSC_2778.jpg"
-                alt="Landlord Image"
-                className="ld-img"
-                objectFit="cover"
-                layout="fill"
-                priority={true}
-              />
-            </div>
-            <div className="editBtns-note">
-              <div className="editBtns">
-                <Button
-                  disableElevation
-                  className="changeAvatar"
-                  variant="contained"
-                >
-                  Change Avatar
-                </Button>
-                <Button
-                  disableElevation
-                  className="deleteAvatar"
-                  variant="contained"
-                >
-                  delete Avatar
-                </Button>
-              </div>
-              <Typography className="note" variant="h6" component="h6">
-                *If you will delete your avatar you will lose your trusted
-                status
-              </Typography>
-            </div>
-          </div>
-        </div>
-        <div className="primaryInfo">
-          <Typography className="title" variant="h5" component="h5">
-            Primary Information
-          </Typography>
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-            className="primaryInformationForm"
-          >
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <TextField
-                label="First name"
-                className="infoInput"
-                id="standard-size-normal"
-                defaultValue="Normal"
-                variant="standard"
-                InputProps={{ disableUnderline: true }}
-              />
-              <TextField
-                className="infoInput"
-                label="Last Name"
-                id="standard-size-normal"
-                defaultValue="Normal"
-                variant="standard"
-                InputProps={{ disableUnderline: true }}
-              />
-            </div>
-            <div>
-              <TextField
-                className="infoInput"
-                label="Location"
-                id="standard-size-normal"
-                defaultValue="Normal"
-                variant="standard"
-                InputProps={{ disableUnderline: true }}
-              />
-            </div>
-            <div>
-              <Typography variant="h6" component="h6">
-                Turn on GPS location
-                <Switch color="primary" />
-              </Typography>
-            </div>
-          </Box>
-        </div>
-        <div className="secondaryInfo">
-          <Typography className="title" variant="h5" component="h5">
-            Secondary Information
-          </Typography>
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-            className="secondaryInformationForm"
-          >
-            <div>
-              <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <Select
-                  value={1}
-                  displayEmpty
-                  inputProps={{ "aria-label": "Without label" }}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-                <FormHelperText>Without label</FormHelperText>
-              </FormControl>
-            </div>
-            <div>
-              <TextField
-                className="infoInput"
-                label="Bio"
-                id="standard-size-normal"
-                defaultValue="Normal"
-                variant="standard"
-                InputProps={{ disableUnderline: true }}
-              />
-            </div>
-            <div>
-              <Button
-                disableElevation
-                className="deleteAvatar"
-                variant="contained"
-              >
-                Save changes
-              </Button>
-            </div>
-          </Box>
-        </div>
+
+        {profileContent}
       </div>
     </Auth>
   );
