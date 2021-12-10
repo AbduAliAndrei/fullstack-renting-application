45a495f556dbad4bc5043be0cd54925908e8c004
andreicristea <w61rab@inf.elte.hu>
2021-12-10 06:28:11 +0100

    fix: filterElement.ts type and add filters state for FilterTool.tsx

--- components/FilterTool.tsx
+++ components/FilterTool.tsx
@@ -1,5 +1,5 @@
 // import '../styles/App.css'
-import React from "react";
+import React, { useState } from "react";
 import { faSortDown, faSortUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -7,6 +7,7 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
 import { fetchCall, RequestType } from "../api/data-fetcher";
 import { AllowedFilterOfferKeys } from "../enums/allowed-offer-keys";
+import { FilterElement, Filters } from "../interfaces/filterElement";
 
 export default function FilterTool() {
   const filterOffers = async () => {
@@ -19,6 +20,84 @@ export default function FilterTool() {
       ],
     });
   };
+
+  const [filters, setFilters] = useState<Filters[]>([
+    {
+      name: "City",
+      filters: [
+        {
+          name: "Budapest",
+          checked: false,
+          value: "Budapest",
+        },
+        {
+          name: "Szeged",
+          checked: false,
+          value: "Szeged",
+        },
+        {
+          name: "Szeged",
+          checked: false,
+          value: "Szeged",
+        },
+      ],
+    },
+    {
+      name: "Price",
+      filters: [
+        {
+          name: "Under 50,000ft",
+          checked: false,
+          value: { lowerBound: 0, upperBound: 50000 },
+        },
+        {
+          name: "50,000 - 100,000ft",
+          checked: false,
+          value: { lowerBound: 50000, upperBound: 100000 },
+        },
+        {
+          name: "100,000 - 150,000ft",
+          checked: false,
+          value: { lowerBound: 100000, upperBound: 150000 },
+        },
+        {
+          name: "150,000 - 250,000ft",
+          checked: false,
+          value: { lowerBound: 150000, upperBound: 2150000 },
+        },
+        {
+          name: "All included",
+          checked: false,
+          value: { lowerBound: -Infinity, upperBound: +Infinity },
+        },
+      ],
+    },
+    {
+      name: "District",
+      filters: [
+        {
+          name: "XII",
+          checked: false,
+          value: "XII",
+        },
+        {
+          name: "XIII",
+          checked: false,
+          value: "XIII",
+        },
+        {
+          name: "IX",
+          checked: false,
+          value: "IX",
+        },
+        {
+          name: "III",
+          checked: false,
+          value: "III",
+        },
+      ],
+    },
+  ]);
 
   return (
     <div className="FilterTool">
